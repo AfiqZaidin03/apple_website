4bc6ad4147a203b6dd89c82a92e82b44f3822fbb
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-07-27 15:33:52 +0800

    [19] Fix video move

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -22,6 +22,12 @@ const VideoCarousel = () => {
   const {isEnd, isLastVideo, startPlay, videoId, isPlaying} = video;
 
   useGSAP(() => {
+    gsap.to('#slider', {
+      transform: `translateX(${-100 * videoId}%)`,
+      duration: 2,
+      ease: 'power2.inOut',
+    })
+
     gsap.to('#video', {
       scrollTrigger: {
         trigger: '#video',
@@ -92,7 +98,7 @@ const VideoCarousel = () => {
       }
 
       const animeUpdate = () => {
-        anim.progress(videoRef.current[videoId] / 
+        anim.progress(videoRef.current[videoId].currentTime / 
         hightlightsSlides[videoId].videoDuration)
       }
   
@@ -145,6 +151,11 @@ const VideoCarousel = () => {
                 muted 
                 ref={(el) => (videoRef.current[i] = 
                 el)} 
+                onEnded={()=> 
+                  i !== 3
+                  ? handleProcess('video-end', i)
+                  : handleProcess('video-last')
+                }
                 onPlay={() => {
                   setVideo((prevVideo) => ({
                     ...prevVideo, isPlaying: true
