f9269f1ce55de185646f1cd74e78cfc1fd462c33
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-07-27 17:35:20 +0800

    [28] Call IPhone and Lights in modelView

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,10 +1,29 @@
+import { PerspectiveCamera, View } from "@react-three/drei"
+import Lights from "./Lights"
+import IPhone from './IPhone';
+import { Suspense } from "react";
 
-
-const ModelView = () => {
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
   return (
-    <div>
-      modelview
-    </div>
+   <View
+    index={index}
+    id={gsapType}
+    className={`border-2 border-red-500 w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
+   >
+    {/* Ambient Light */}
+    <ambientLight intensity={0.3} />
+
+    <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+    <Lights />
+    <Suspense fallback={<div>Loading</div>}>
+        <IPhone 
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+        />
+    </Suspense>
+   </View>
   )
 }
 
